2996ae67de8eda9c5e576fee6f7e315d90b6ab75
Amit Singh <amit.reflectiontp@gmail.com>
2022-08-20 21:15:39 +1030

    class-constructor

---
+++ 06082022 class,constructor/class-constructor.js
@@ -0,0 +1,25 @@
+class slider{
+    constructor(){
+        
+    }
+    prev = () => {
+        alert('prev'); 
+    }
+    autoslide = () => {
+        alert('autoslide'); 
+    }
+    next = () => {       
+        alert('next');
+    }
+    
+    
+   
+    
+}
+let show = new slider();
+
+
+/*document.getElementById('day').innerHTML = week;
+
+this.name = 'Narayan'
+        alert(this.name);*/
